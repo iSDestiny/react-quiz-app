1dabfc8070c15ff243314c1e99c522728e9d9fce
Jason <jbugallo@uci.edu>
2020-03-18 13:30:33 -0700

    Updates

---
+++ precache-manifest.1edcb254e900afc5e7386528f13090c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6917439352e19ce4635aba49484bf09",
+    "url": "/react-quiz-app/index.html"
+  },
+  {
+    "revision": "bfd68a4a3bea0d9a871d",
+    "url": "/react-quiz-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "abad47590157af1a8f74",
+    "url": "/react-quiz-app/static/js/2.1decf9e8.chunk.js"
+  },
+  {
+    "revision": "a5288a4ed4e4beba8413aa5c9ae969ea",
+    "url": "/react-quiz-app/static/js/2.1decf9e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfd68a4a3bea0d9a871d",
+    "url": "/react-quiz-app/static/js/main.0726a758.chunk.js"
+  },
+  {
+    "revision": "dba07ab58722f79e744f",
+    "url": "/react-quiz-app/static/js/runtime-main.eb0a942a.js"
+  }
+]);
